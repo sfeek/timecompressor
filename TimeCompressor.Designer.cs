922ee24d847cd9c361212b48088fc727be2ae12b
Shane Feek <shane.feek@gmail.com>
2020-10-09 10:19:27 -0400

    First Commit

---
+++ TimeCompressor.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Time_Compressor
+{
+    partial class TimeCompressor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimeCompressor));
+            this.btn_compress = new System.Windows.Forms.Button();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblRecordNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_compress
+            // 
+            this.btn_compress.Location = new System.Drawing.Point(53, 57);
+            this.btn_compress.Name = "btn_compress";
+            this.btn_compress.Size = new System.Drawing.Size(206, 62);
+            this.btn_compress.TabIndex = 0;
+            this.btn_compress.Text = "Time Compress";
+            this.btn_compress.UseVisualStyleBackColor = true;
+            this.btn_compress.Click += new System.EventHandler(this.btn_compress_Click);
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(99, 12);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(42, 20);
+            this.txtCount.TabIndex = 1;
+            this.txtCount.Text = "60";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(51, 15);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(42, 13);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "Interval";
+            // 
+            // lblRecordNumber
+            // 
+            this.lblRecordNumber.AutoSize = true;
+            this.lblRecordNumber.Location = new System.Drawing.Point(162, 15);
+            this.lblRecordNumber.Name = "lblRecordNumber";
+            this.lblRecordNumber.Size = new System.Drawing.Size(52, 13);
+            this.lblRecordNumber.TabIndex = 3;
+            this.lblRecordNumber.Text = "Record #";
+            // 
+            // TimeCompressor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 136);
+            this.Controls.Add(this.lblRecordNumber);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.btn_compress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "TimeCompressor";
+            this.Text = "Time Compressor v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_compress;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label lblRecordNumber;
+    }
+}
+
---
+++ TimeCompressor.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Time_Compressor
+{
+    public partial class TimeCompressor : Form
+    {
+        public TimeCompressor()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_compress_Click(object sender, EventArgs e)
+        {
+            int interval;
+            int x, y, i;
+            string line;
+            int counter = 0;
+            int icounter = 0;
+            string[] labels;
+            string[] fields;
+            double[,] buffer;
+            double a, s;
+            string output = String.Empty;
+            string outfilename = String.Empty;
+
+            List<double> avg = new List<double>();
+            List<double> std = new List<double>();
+
+            btn_compress.Enabled = false;
+            
+            try
+            {
+                interval = Convert.ToInt32(txtCount.Text);
+            }
+            catch { return; }
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    outfilename = openFileDialog.FileName + "-cmp.csv";
+                    if (File.Exists(outfilename)) File.Delete(outfilename); // Make sure the old file is gone
+
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        line = reader.ReadLine();
+                        labels = ParseLine(line);
+                        buffer = new double[interval, labels.Length];
+
+                        output += "Rec#,";
+                        for (x = 0; x < labels.Length; x++) // Write out our labels
+                        {
+                            if (x < labels.Length - 1)
+                                output += labels[x] + "_avg," + labels[x] + "_std,";
+                            else
+                                output += labels[x] + "_avg," + labels[x] + "_std";
+                        }
+
+                        using (StreamWriter sw = File.AppendText(outfilename))
+                        {
+                            sw.WriteLine(output);
+
+                            counter = 1;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                if (counter % interval == 0) // Calculate values and output them
+                                {
+                                    Application.DoEvents();
+                                    output = String.Empty;
+                                    output += counter + ",";
+
+                                    for (x = 0; x < labels.Length; x++)
+                                    {
+                                        avg.Clear();
+                                        std.Clear();
+
+                                        for (y = 0; y < icounter; y++)
+                                        {
+                                            avg.Add(buffer[y, x]);
+                                            std.Add(buffer[y, x]);
+                                        }
+
+                                        a = avg.Average();
+                                        s = StandardDeviation(a,std);
+
+                                        if (x < labels.Length - 1)
+                                            output += a + "," + s + ",";
+                                        else
+                                            output += a + "," + s;
+                                    }
+
+
+                                    sw.WriteLine(output);
+                                    lblRecordNumber.Text = "Record #" + counter;
+
+                                    icounter = 0;
+                                }
+
+                                for (i = 0; i < labels.Length; i++) // Otherwise add to buffer
+                                {
+                                    fields = ParseLine(line);
+                                    try
+                                    {
+                                        buffer[icounter, i] = Convert.ToDouble(fields[i]);
+                                    }
+                                    catch { buffer[icounter, i] = 0.0; }
+                                }
+
+                                counter++;
+                                icounter++;
+                            }
+
+                            // Process the remaining values
+                            output = String.Empty;
+                            output += counter + ",";
+
+                            for (x = 0; x < labels.Length; x++)
+                            {
+                                avg.Clear();
+                                std.Clear();
+
+                                for (y = 0; y < icounter; y++)
+                                {
+                                    avg.Add(buffer[y, x]);
+                                    std.Add(buffer[y, x]);
+                                }
+
+                                a = avg.Average();
+                                s = StandardDeviation(a, std);
+
+                                if (x < labels.Length - 1)
+                                    output += a + "," + s + ",";
+                                else
+                                    output += a + "," + s;
+                            }
+
+                            sw.WriteLine(output);
+                            lblRecordNumber.Text = "Record #" + counter;
+                            icounter = 0;
+                        }
+                    }
+                }
+            }
+            btn_compress.Enabled = true;
+        }
+
+        // Parse an individual line of CSV
+        static string[] ParseLine(string lines)
+        {
+            string[] fields;
+            string[] lineArray;
+
+            try
+            {
+                fields = Regex.Split(lines, ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+                lineArray = new string[fields.Length];
+
+                int x = 0;
+                foreach (string value in fields)
+                {
+                    string check = Regex.Replace(value, "^\"|\"$", "");
+                    lineArray[x] = check;
+                    x++;
+                }
+            }
+            catch 
+            {
+                return null;
+            }
+
+            return lineArray;
+        }
+
+        // Calculate Standard Deviation
+        static double StandardDeviation(double average,List<double> doubleList)
+        {
+            double sumOfDerivation = 0;
+
+            foreach (double value in doubleList)
+            {
+                sumOfDerivation += Math.Pow(value - average, 2);
+            }
+
+            return Math.Sqrt(sumOfDerivation / (doubleList.Count - 1));
+        }
+    }
+}
